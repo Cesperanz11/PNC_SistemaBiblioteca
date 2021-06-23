a85bbe0be4b165e212aada8cb9c5a5266ef10a4e
Cesperanz11 <00077618@uca.edu.sv>
2021-06-23 11:28:23 -0600

    Agregando entidad Usuario y terminando entidad Rol

--- SistemaBiblioteca/Sistema.Entidades/Rol.cs
+++ SistemaBiblioteca/Sistema.Entidades/Rol.cs
@@ -8,5 +8,14 @@ namespace Sistema.Entidades
 {
     class Rol
     {
+        //Creando las variables que actuaran como los atributos de la tabla Rol
+        public int id_rol { set; get; }
+
+        public string nombre { set; get; }
+
+        public string descripcion { set; get; }
+
+        public bool estado { set; get; }
+
     }
 }
---
+++ SistemaBiblioteca/Sistema.Entidades/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidades
+{
+    class Usuario
+    {
+        //Creando las variables que actuaran como los atributos de la tabla Usuario
+        public int id_usuario { set; get; }
+
+        public string nombre { set; get; }
+
+        public string apellido { set; get; }
+
+        public string telefono { set; get; }
+
+        public string email { set; get; }
+
+        public bool estdo { set; get; }
+
+        public int id_rol { set; get; }
+    }
+}
