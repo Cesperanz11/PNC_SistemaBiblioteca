629d9bec02228cdb6ca0046142fe9195f314bcc9
Cesperanz11 <00077618@uca.edu.sv>
2021-06-23 17:42:22 -0600

    Creando procedimientos de DUsuario

--- SistemaBiblioteca/Sistema.Datos/DUsuario.cs
+++ SistemaBiblioteca/Sistema.Datos/DUsuario.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Sistema.Datos
 {
@@ -13,5 +15,40 @@ namespace Sistema.Datos
         // Funcion de verificar existencia de usuario
 
         //Funcion de listar usuarios (maestros)
+        public DataTable Listar()
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //para obtener la conexion con la BD, se llaman las funciones en la clase Conexion
+                SqlCon = Conexion.getInstancia().CrearConexion();
+
+                //para tener los datos se necesita llama un procedimiento almacenado "usuario_listar" dentro de SQL Server
+                SqlCommand Comando = new SqlCommand("usuario_listar", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+
+                //se abre la conexion con BD
+                SqlCon.Open();
+
+                //ejecutando el procedimiento almacenado
+                Resultado = Comando.ExecuteReader();
+
+                //llenando la tabla con la informacion de la ejecucion del comando
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //si se genera conexion con exito, luego de tener los datos se cierra
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
     }
 }
