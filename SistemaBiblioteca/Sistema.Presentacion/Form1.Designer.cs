6218a648fc1c29a949f7df223c907e8cd3ddb77a
Cesperanz11 <00077618@uca.edu.sv>
2021-06-23 18:39:12 -0600

    Creando Form de Login y capa de presentacion de login

--- SistemaBiblioteca/Sistema.Presentacion/Form1.Designer.cs
+++ SistemaBiblioteca/Sistema.Presentacion/Form1.Designer.cs
@@ -29,13 +29,55 @@ namespace Sistema.Presentacion
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TxtEmail = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtEmail
+            // 
+            this.TxtEmail.Location = new System.Drawing.Point(308, 104);
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.Size = new System.Drawing.Size(131, 20);
+            this.TxtEmail.TabIndex = 0;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(308, 151);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(131, 20);
+            this.TxtPassword.TabIndex = 1;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(308, 222);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(131, 23);
+            this.BtnLogin.TabIndex = 2;
+            this.BtnLogin.Text = "Ingresar";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtEmail);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TxtEmail;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button BtnLogin;
     }
 }
 
--- SistemaBiblioteca/Sistema.Presentacion/Form1.cs
+++ SistemaBiblioteca/Sistema.Presentacion/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sistema.Negocio;
+
 
 namespace Sistema.Presentacion
 {
@@ -16,5 +18,82 @@ namespace Sistema.Presentacion
         {
             InitializeComponent();
         }
+
+        //Funcion de Mensaje de Error
+        private void MensajeError(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema Biblioteca", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        //Funcion de Mensaje de OK
+        private void MensajeOk(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema Biblioteca", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //Funcion de Limpiar formulario
+        private void Limpiar()
+        {
+            TxtEmail.Clear();
+            TxtPassword.Clear();
+        }
+
+        //Funcion de Login y Verificacion
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string email = TxtEmail.Text;
+            string password = TxtPassword.Text;
+
+            try
+            {
+                string Rpta = "";
+
+                //Validando que ninguno de los datos importantes esten vacios
+                if (email == string.Empty)
+                {
+                    this.MensajeError("Faltan datos por ingresar.");
+                    //Mostrar Error Provider donde faltan datos
+                    //ErrorIcono.SetError(txtNombre, "Ingrese un nombre");
+                }
+                else if (password == string.Empty)
+                {
+                    this.MensajeError("Faltan datos por ingresar.");
+                    //Mostrar Error Provider donde faltan datos
+                    //ErrorIcono.SetError(txtApellido, "Ingrese un apellido");
+                }
+                else
+                {
+                    //Verificar si el usuario que hemos ingresado existe realmente en la BD
+                    Rpta = NUsuario.Verificar(email.Trim());
+                    if (Rpta.Equals("OK")) //El usuario si existe en la BD
+                    {
+                        //Verificar si la password que hemos ingresado coincide con las credenciales en la BD
+                        Rpta = NUsuario.Login(email.Trim(), password.Trim());
+                        if (Rpta.Equals("OK")) //La password es correcta
+                        {
+                            this.MensajeOk("Se hizo un login exitosamente"); //Mostrarle al usuario que si se hacer login
+                                                                             
+                        }
+                        else //La password es incorrecta
+                        {
+                            this.MensajeError(Rpta);
+                            this.Limpiar(); //Borrar la informacion de los TextBoxs
+                        }
+                    }
+                    else //El usuario no existe en la BD
+                    {
+                        this.MensajeError(Rpta);
+                        this.Limpiar(); //Borrar la informacion de los TextBoxs
+                    }
+                    }
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
     }
+    
 }
