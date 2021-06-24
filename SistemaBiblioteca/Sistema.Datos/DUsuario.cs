3d5388043c33a15d1df6646c507e751ac1746880
Cesperanz11 <00077618@uca.edu.sv>
2021-06-23 18:01:35 -0600

    Creando CRUD de Usuario

--- SistemaBiblioteca/Sistema.Datos/DUsuario.cs
+++ SistemaBiblioteca/Sistema.Datos/DUsuario.cs
@@ -11,8 +11,97 @@ namespace Sistema.Datos
     class DUsuario
     {
         // Funcion de login
+        public string Login(string Email, string Password)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //creando conexion con la BD
+                SqlCon = Conexion.getInstancia().CrearConexion();
+
+                //generando nuevo comando para ejecutar en SQL Server el procedimiento almacenado "usuario_login"
+                SqlCommand Comando = new SqlCommand("usuario_login", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+
+                //Declarando value de los parametros dentro del procedimiento almacenado
+                Comando.Parameters.Add("@email", SqlDbType.VarChar).Value = Email;
+                Comando.Parameters.Add("@password", SqlDbType.VarChar).Value = Password;
+
+                //consulta para ejecutar el procedimiento almacenado
+                SqlParameter ParExiste = new SqlParameter();
+                ParExiste.ParameterName = "@existe";
+                //especificando el tipo de variable que va retornar
+                ParExiste.SqlDbType = SqlDbType.Int;
+
+                //retornando el valor de respuesta de la query
+                ParExiste.Direction = ParameterDirection.Output;
+                Comando.Parameters.Add(ParExiste);
+
+                //se abre la conexion con BD
+                SqlCon.Open();
+
+                //ejecutando query que no devuelve ningun valor
+                Comando.ExecuteNonQuery();
+                Rpta = Convert.ToString(ParExiste.Value);
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                //si se genera conexion con exito, luego de tener los datos se cierra
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
 
         // Funcion de verificar existencia de usuario
+        public string Existe(string Email)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //creando conexion con la BD
+                SqlCon = Conexion.getInstancia().CrearConexion();
+
+                //generando nuevo comando para ejecutar en SQL Server el procedimiento almacenado "usuario_existe"
+                SqlCommand Comando = new SqlCommand("usuario_existe", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+
+                //Declarando value de los parametros dentro del procedimiento almacenado
+                Comando.Parameters.Add("@email", SqlDbType.VarChar).Value = Email;
+
+                //consulta para ejecutar el procedimiento almacenado
+                SqlParameter ParExiste = new SqlParameter();
+                ParExiste.ParameterName = "@existe";
+                //especificando el tipo de variable que va retornar
+                ParExiste.SqlDbType = SqlDbType.Int;
+
+                //retornando el valor de respuesta de la query
+                ParExiste.Direction = ParameterDirection.Output;
+                Comando.Parameters.Add(ParExiste);
+
+                //se abre la conexion con BD
+                SqlCon.Open();
+
+                //ejecutando query que no devuelve ningun valor
+                Comando.ExecuteNonQuery();
+                Rpta = Convert.ToString(ParExiste.Value);
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                //si se genera conexion con exito, luego de tener los datos se cierra
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
 
         //Funcion de listar usuarios (maestros)
         public DataTable Listar()
